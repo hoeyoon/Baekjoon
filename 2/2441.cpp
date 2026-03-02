a82b1b216193bb7e47c4a7f24e0374962ada3d3a
hoeyoon <hoeiyoon57@gmail.com>
2026-03-02 16:27:33 +0900

    C++ 별 찍기 - 4

--- 2/2441.cpp
+++ 2/2441.cpp
@@ -23,3 +23,22 @@ https://www.acmicpc.net/problem/2441
    **
     *
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        for(int j = 0; j < n - i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
