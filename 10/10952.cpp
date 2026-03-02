9a8ecb07a76917f675ae952e5b7f5e28bd2209fe
hoeyoon <hoeiyoon57@gmail.com>
2026-03-02 19:08:46 +0900

    C++ A+B - 5

--- 10/10952.cpp
+++ 10/10952.cpp
@@ -30,3 +30,19 @@ https://www.acmicpc.net/problem/10952
 17
 7
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    while(1){
+        int a, b;
+        cin >> a >> b;
+        if(a == 0 && b == 0){
+            break;
+        }
+
+        cout << a + b << endl;
+    }
+    return 0;
+}
