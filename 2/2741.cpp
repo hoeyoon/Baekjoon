f6990ae336438609c28f779f1e8210933b336035
hoeyoon <hoeiyoon57@gmail.com>
2026-03-02 15:17:45 +0900

    C++ N 찍기

--- 2/2741.cpp
+++ 2/2741.cpp
@@ -21,3 +21,17 @@ https://www.acmicpc.net/problem/2741
 4
 5
 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        //cout << i + 1 << endl; // 시간초과
+        cout << i + 1 << '\n';
+    }
+    return 0;
+}
