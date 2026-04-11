bd66b83857b8418a7ac4088612faa73236b7e33b
hoeyoon <89262375+hoeyoon@users.noreply.github.com>
2026-04-11 21:07:14 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ C++17/백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    queue<int> q;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        q.push(i + 1);
+    }
+
+    while(q.size() != 1){
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front() << endl;
+    return 0;
+}
